aa313d5ba13ff3fd4ce568a189d0505390f03d98
Patrick Holton <patrickholton@Patricks-MacBook-Pro.local>
2023-04-21 20:49:53 -0600

    index added

---
+++ pages/index.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Image from "next/image";
+import logo from "../public/images/logo2.png";
+import Link from "next/link";
+const index = () => {
+  return (
+    <div className="bg-black h-screen w-screen flex justify-center items-center flex-col">
+      <div className="w-[30%] aspect-[1630/575] rounded-full relative ">
+        <Image
+          alt="logo"
+          src={logo}
+          fill
+          objectFit="contain"
+          className="rounded-full"
+        />
+      </div>
+
+      <Link
+        href={"/product/test"}
+        className="bg-main-500 text-white text-3xl py-1 px-4 rounded-md hover:bg-black"
+      >
+        Shop Now
+      </Link>
+    </div>
+  );
+};
+
+export default index;
